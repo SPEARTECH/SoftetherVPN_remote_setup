df8572c8304dd75f5384daf23b1e5a5035fcc589
SPEARTECH <70530918+SPEARTECH@users.noreply.github.com>
2024-11-06 15:18:23 -0500

    Update create_server.sh

--- create_server.sh
+++ create_server.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-Wget https://www.softether-download.com/files/softether/v4.43-9799-beta-2023.08.31-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.43-9799-beta-2023.08.31-linux-x64-64bit.tar.gz
-tar xzvf softether-vpnserver-v4.43-9799-beta-2023.08.31-linux-x64-64bit.tar.gz
+
+HUBNAME=""
+USERNAME=""
+INTERFACE=""
+
+cd /usr/local
+sudo apt-get update
+
+wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.34-9745-beta/softether-vpnserver_v4.34-9745-beta-2020.04.05-linux-x64-64bit.tar.gz
+tar xzvf softether-vpnserver_v4.34-9745-beta-2020.04.05-linux-x64-64bit.tar.gz
+
 cd vpnserver
-sudo make
+make
+
+sudo ./vpncmd localhost /SERVER /CMD HubCreate $HUBNAME
+sudo ./vpncmd localhost /SERVER /CMD UserCreate $USERNAME
+sudo ./vpncmd localhost /SERVER /CMD UserPasswordSet $USERNAME /PASSWORD:$PASSWORD
+sudo ./vpncmd localhost /SERVER /CMD BridgeCreate $HUBNAME /DEVICE:$INTERFACE
+
+# Enable SecureNAT in the VPNCMD prompt:
+# This will allow VPN clients to be assigned virtual IP addresses and route traffic as if they are part of the same network.
+sudo ./vpncmd localhost /SERVER /CMD SecureNatEnable
 sudo ./vpnserver start
-sudo ./vpncmd
-# 1
-# GroupCreate <group name>
-# HubCreate <hub name>
-# UserCreate <user name>
-# UserPasswordSet <password>
-# SecureNatEnable
+# cp /root/SCCT-LAN-Breakout/signalserver/scct.service /etc/systemd/system/
+
